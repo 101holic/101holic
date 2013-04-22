d0e9979aa09ca9797200faef4d0e8579157a9b7a
Sebastien Guignot <kxu@tsys.org>
2013-04-22 02:06:58 +0200

    Initial commit, rails 4 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fiveholic::Application.config.secret_key_base = '1b35533b36cddda539a45584e4e5edeef37b66ae7d3c38f7fcccc829e1c63e0261f0138f46682875cd78e1deed484494f6d35be210880ca7bed66a4464687317'
