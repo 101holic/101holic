2da339615788e7e11cc271d05b257ab6183a3c25
Sebastien Guignot <kxu@tsys.org>
2013-05-04 17:27:42 +0200

    Update rails 4-beta1 to 4-rc1.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Fiveholic::Application.config.session_store :encrypted_cookie_store, key: '_fiveholic_session'
+Fiveholic::Application.config.session_store :cookie_store, key: '_fiveholic_session'
